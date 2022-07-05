a713b9bf8552a91cf9397c2fd1ba08973b11187a
pisarevkir <102020833+pisarevkir@users.noreply.github.com>
2022-07-05 10:34:15 +0300

    auth module (#31)
    Removed Account modules + account api requests
    Simplified Auth module code
    Added `Authorization` header for authenticated users

--- src/store/modules/Auth/sagas/index.ts
+++ src/store/modules/Auth/sagas/index.ts
@@ -4,8 +4,7 @@ import {AppCommonActions} from '@store/modules/AppCommon/actions';
 import {AuthActions} from '@store/modules/Auth/actions';
 import {all, takeLatest} from 'redux-saga/effects';
 
-import {checkUserSaga} from './checkUser';
-import {getTokenSaga} from './getToken';
+import {loadUserSaga} from './loadUser';
 import {signInEmailSaga} from './signInEmail';
 import {signInPhoneSaga} from './signInPhone';
 import {signInSocialSaga} from './signInSocial';
@@ -13,8 +12,7 @@ import {signOutSaga} from './signOut';
 
 export function* rootAuthSaga() {
   yield all([
-    takeLatest(AuthActions.GET_TOKEN.START.type, getTokenSaga),
-    takeLatest(AppCommonActions.APP_LOADED.STATE.type, checkUserSaga),
+    takeLatest(AppCommonActions.APP_LOADED.STATE.type, loadUserSaga),
     takeLatest(AuthActions.SIGN_IN_EMAIL.START.type, signInEmailSaga),
     takeLatest(AuthActions.SIGN_IN_PHONE.START.type, signInPhoneSaga),
     takeLatest(AuthActions.SIGN_IN_SOCIAL.START.type, signInSocialSaga),
