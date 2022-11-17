296bc53a4a6da5754c3f063bd9aad1f15200c247
ice-minos <alexandr.zabrodskyi@leftclick.io>
2022-11-17 16:36:39 +0200

    chore: adds a redirect to the user profile when tap on avatar

--- src/screens/HomeFlow/Home/components/Header/index.tsx
+++ src/screens/HomeFlow/Home/components/Header/index.tsx
@@ -48,11 +48,14 @@ export const HomeHeader = memo(({translateY, transitionOffset}: Props) => {
     <View style={[styles.container, topOffset.current]}>
       <View style={styles.body}>
         {user?.profilePictureUrl && (
-          <Avatar
-            uri={user.profilePictureUrl}
-            size={rem(36)}
-            borderRadius={rem(16)}
-          />
+          <Touchable onPress={onGreetingPress}>
+            <Avatar
+              uri={user.profilePictureUrl}
+              size={rem(36)}
+              borderRadius={rem(16)}
+              allowFullScreen={false}
+            />
+          </Touchable>
         )}
         <Animated.View style={[styles.greeting, fromAnimatedStyle]}>
           <Touchable onPress={onGreetingPress}>
