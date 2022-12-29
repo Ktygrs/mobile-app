671b6d4c7eb897147cd147b2340fbe60080a3530
ice-orion <102020833+ice-orion@users.noreply.github.com>
2022-12-29 12:31:50 +0400

    non serializable redux warning (#94)

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -18,7 +18,16 @@ if (__DEV__) {
 
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: middlewares,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      // https://redux-toolkit.js.org/usage/usage-guide#working-with-non-serializable-data
+      serializableCheck: {
+        ignoredActionPaths: ['payload.raceConditionStrategy'],
+        ignoredPaths: [
+          'utilityProcessStatuses.UPDATE_ACCOUNT.payload.raceConditionStrategy',
+        ],
+      },
+    }).concat(middlewares),
   enhancers: [loggingReduxEnhancer],
 });
 
