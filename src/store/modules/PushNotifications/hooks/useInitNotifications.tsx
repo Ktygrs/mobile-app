2f65f1e61aaab7f1277434597300f60fa2d12389
ice-hades <ice-hades@users.noreply.github.com>
2023-02-19 17:33:53 +0300

    chore: added firese notification handlers

--- src/store/modules/PushNotifications/hooks/useInitNotifications.tsx
+++ src/store/modules/PushNotifications/hooks/useInitNotifications.tsx
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: BUSL-1.1
 
 import {ENV} from '@constants/env';
+import type {FirebaseMessagingTypes} from '@react-native-firebase/messaging';
+import messaging from '@react-native-firebase/messaging';
 import {DeviceActions} from '@store/modules/Devices/actions';
+import {LinkingActions} from '@store/modules/Linking/actions';
 import {isPermissionGrantedSelector} from '@store/modules/Permissions/selectors';
 import {useEffect} from 'react';
 import ReactMoE from 'react-native-moengage/src/index';
@@ -36,4 +39,39 @@ export function useInitNotifications() {
        */
     ReactMoE.initialize(ENV.MO_ENGAGE_APP_ID ?? '');
   }, [dispatch, hasPushPermissions]);
+
+  useEffect(() => {
+    messaging().getToken().then(console.log);
+    const handleMessage = (
+      message: FirebaseMessagingTypes.RemoteMessage | null,
+    ) => {
+      console.log('handleMessage', message?.data, message?.data);
+      if (message?.data?.deeplink) {
+        dispatch(
+          LinkingActions.HANDLE_URL.STATE.create(message?.data?.deeplink, true),
+        );
+      }
+    };
+    /*
+         When a notification from FCM has triggered the application to open from a quit state,
+         this method will return a `RemoteMessage` containing the notification data, or `null` if
+         the app was opened via another method.
+         */
+    messaging().getInitialNotification().then(handleMessage);
+    /*
+      When the user presses a notification displayed via FCM, this listener will be called if the app
+      has opened from a background state.
+      */
+    const unsubscribeFromOnNotificationOpenedApp =
+      messaging().onNotificationOpenedApp(handleMessage);
+
+    /*
+     * When any FCM payload is received, the listener callback is called with a `RemoteMessage`.
+     */
+    const unsubscribeFromOnMessage = messaging().onMessage(handleMessage);
+    return () => {
+      unsubscribeFromOnMessage();
+      unsubscribeFromOnNotificationOpenedApp();
+    };
+  }, [dispatch]);
 }
