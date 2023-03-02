48db1a18598ee43c4006a2d0f2d4cb822fc8c744
ice-hector <96414297+ice-hector@users.noreply.github.com>
2023-03-02 10:43:21 +0200

    chore: fix start mining action handling

--- src/store/modules/Achievements/sagas/completeStartMiningAchievementSaga.ts
+++ src/store/modules/Achievements/sagas/completeStartMiningAchievementSaga.ts
@@ -1,19 +1,23 @@
 // SPDX-License-Identifier: BUSL-1.1
 
+import {AchievementsActions} from '@store/modules/Achievements/actions';
 import {getAchievements} from '@store/modules/Achievements/selectors/getAchievements';
-import {select} from 'redux-saga/effects';
+import {hasUncompletedAchievements} from '@store/modules/Achievements/selectors/hasUncompletedAchievements';
+import {put, select} from 'redux-saga/effects';
 
 export function* completeStartMiningAchievementSaga() {
   const achievements: ReturnType<typeof getAchievements> = yield select(
     getAchievements,
   );
 
-  console.log(achievements);
+  const hasUncompleted: ReturnType<typeof hasUncompletedAchievements> =
+    yield select(hasUncompletedAchievements);
 
-  //   const miningAchievement = achievements.find(
-  //     achievement => achievement.type === 'start_mining',
-  //   );
-  //   if (hasUncompleted && miningAchievement && !miningAchievement.completed) {
-  //    //dispatch achievement complete
-  //   }
+  const miningAchievement = achievements.find(
+    achievement => achievement.type === 'start_mining',
+  );
+
+  if (hasUncompleted && miningAchievement && !miningAchievement.completed) {
+    yield put(AchievementsActions.COMPLETE_NEXT_ACHIEVEMENT.STATE.create());
+  }
 }
