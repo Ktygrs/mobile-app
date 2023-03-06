5c2cd2a1e040300564c15dbf9285bf185d483eff
ice-hector <96414297+ice-hector@users.noreply.github.com>
2023-03-06 17:45:04 +0200

    chore: move titles descriptions and icon bg colors to separate file

--- src/screens/HomeFlow/Home/components/Achievements/achievements.tsx
+++ src/screens/HomeFlow/Home/components/Achievements/achievements.tsx
@@ -2,7 +2,14 @@
 
 import {AchievementType} from '@api/achievements/types';
 import {COLORS} from '@constants/colors';
+import {InviteIcon} from '@svg/InviteIcon';
+import {LogoIcon} from '@svg/LogoIcon';
+import {TelegramSvg} from '@svg/Telegram';
+import {TwitterIcon} from '@svg/TwitterIcon';
+import {UserCircleSvg} from '@svg/UserCircle';
+import {VerifiedUserSvg} from '@svg/VerifiedUser';
 import {t} from '@translations/i18n';
+import React from 'react';
 
 export type AchievementInfo = {
   title: string;
@@ -41,4 +48,29 @@ export const achievements: {[key in AchievementType]: AchievementInfo} = {
     description: t('home.steps.step_six.description'),
     iconBgColor: COLORS.blueViolet,
   },
+};
+
+export const iconForType = (type: AchievementType) => {
+  let icon = null;
+  switch (type) {
+    case 'claim_username':
+      icon = <VerifiedUserSvg />;
+      break;
+    case 'start_mining':
+      icon = <LogoIcon color={COLORS.white} width={24} height={24} />;
+      break;
+    case 'upload_profile_picture':
+      icon = <UserCircleSvg />;
+      break;
+    case 'follow_us_on_twitter':
+      icon = <TwitterIcon width={20} height={20} fill={COLORS.white} />;
+      break;
+    case 'join_telegram':
+      icon = <TelegramSvg />;
+      break;
+    case 'invite_friends':
+      icon = <InviteIcon fill={COLORS.white} width={21} height={20} />;
+      break;
+  }
+  return icon;
 };
