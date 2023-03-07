2285d0774110e8824b74aa67503ac663c66a38e1
ice-hector <96414297+ice-hector@users.noreply.github.com>
2023-03-07 19:03:26 +0200

    chore: connect get tasks api

--- src/api/tasks/getTasks.ts
+++ src/api/tasks/getTasks.ts
@@ -1,18 +1,13 @@
 // SPDX-License-Identifier: BUSL-1.1
 
-import {mockedTasks} from '@api/tasks/mockData';
+import {get} from '@api/client';
 
-// import {get} from '@api/client';
 import {Task} from './types';
 
 /**
  * Returns an user tasks
  */
 
-export function getTasks(): Promise<Task[]> {
-  /** Mocked data */
-  return new Promise(resolve => {
-    resolve(mockedTasks);
-  });
-  // return get<Task[]>('/achievements').then(() => {});
+export function getTasks(userId: string): Promise<Task[]> {
+  return get<Task[]>(`/tasks/x/users/${userId}`);
 }
--- src/store/modules/Tasks/sagas/loadTasksSaga.ts
+++ src/store/modules/Tasks/sagas/loadTasksSaga.ts
@@ -1,14 +1,19 @@
 // SPDX-License-Identifier: BUSL-1.1
 
 import {Api} from '@api/index';
+import {userIdSelector} from '@store/modules/Account/selectors';
 import {TasksActions} from '@store/modules/Tasks/actions';
 import {getErrorMessage} from '@utils/errors';
-import {call, put, SagaReturnType} from 'redux-saga/effects';
+import {call, put, SagaReturnType, select} from 'redux-saga/effects';
 
 export function* loadTasksSaga() {
+  const userId: ReturnType<typeof userIdSelector> = yield select(
+    userIdSelector,
+  );
   try {
     const tasks: SagaReturnType<typeof Api.tasks.getTasks> = yield call(
       Api.tasks.getTasks,
+      userId,
     );
     yield put(TasksActions.GET_TASKS.SUCCESS.create({tasks}));
   } catch (error) {
