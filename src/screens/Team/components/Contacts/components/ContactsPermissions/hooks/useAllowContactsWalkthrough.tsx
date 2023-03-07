8e2c5e7f804e4d61785226ea4f10598dce1f4ddc
ice-orion <102020833+ice-orion@users.noreply.github.com>
2023-03-07 12:45:50 +0800

    chore: go next on allow contacts access

--- src/screens/Team/components/Contacts/components/ContactsPermissions/hooks/useAllowContactsWalkthrough.tsx
+++ src/screens/Team/components/Contacts/components/ContactsPermissions/hooks/useAllowContactsWalkthrough.tsx
@@ -2,20 +2,39 @@
 
 import {AllowContactsButton} from '@screens/Team/components/Contacts/components/ContactsPermissions/components/AllowContactsButton';
 import {WalkThroughElementContainer} from '@screens/WalkThrough/components/WalkThroughElementContainer';
+import {isPermissionGrantedSelector} from '@store/modules/Permissions/selectors';
+import {WalkThroughActions} from '@store/modules/WalkThrough/actions';
 import {useMeasureWalkthroughElement} from '@store/modules/WalkThrough/hooks/useMeasureWalkthroughElement';
 import {useSetWalkthroughElementData} from '@store/modules/WalkThrough/hooks/useSetWalkthroughElementData';
 import {useEffect} from 'react';
 import React from 'react';
 import {StyleSheet} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
 import {rem} from 'rn-units';
 
 const CONTAINER_PADDING = rem(20);
 
 export const useAllowContactsWalkthrough = () => {
+  const dispatch = useDispatch();
+
   const {setWalkthroughElementData} = useSetWalkthroughElementData();
 
   const {elementRef, elementData, measureElement} =
     useMeasureWalkthroughElement();
+
+  const hasContactsPermissions = useSelector(
+    isPermissionGrantedSelector('contacts'),
+  );
+
+  useEffect(() => {
+    if (hasContactsPermissions) {
+      dispatch(
+        WalkThroughActions.COMPLETE_WALK_THROUGH_STEP.STATE.create({
+          stepKey: 'allowContacts',
+        }),
+      );
+    }
+  }, [dispatch, hasContactsPermissions]);
 
   useEffect(() => {
     if (elementData) {
