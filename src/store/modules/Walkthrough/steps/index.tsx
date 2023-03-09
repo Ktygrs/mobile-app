9623fdd59114c518e4f81f4b2c1682f2d303d370
ice-orion <102020833+ice-orion@users.noreply.github.com>
2023-03-09 13:45:46 +0800

    fix: merge WALKTHROUGH_STEPS

--- src/store/modules/Walkthrough/steps/index.tsx
+++ src/store/modules/Walkthrough/steps/index.tsx
@@ -3,7 +3,7 @@
 import {NEWS_WALKTHROUGH_STEPS} from '@store/modules/Walkthrough/steps/news';
 import {TEAM_WALKTHROUGH_STEPS} from '@store/modules/Walkthrough/steps/team';
 
-export const WALKTHROUGH_STEPS = {
+export const WALKTHROUGH_STEPS = [
   ...TEAM_WALKTHROUGH_STEPS,
   ...NEWS_WALKTHROUGH_STEPS,
-};
+];
